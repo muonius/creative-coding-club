877e6d674cf9b0aa690d58c2c87643fc0be6136f
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-24 11:10:02 -0400

    update

--- simplesine.js
+++ simplesine.js
@@ -8,11 +8,31 @@ function setup() {
 function draw() {
   background(30);
   stroke(255);
-
+  noFill();
   translate(0, 150);
+  beginShape();
   for (let i = 0; i < 359; i++) {
     let x = map(i, 0, 359, 0, width);
-    let y = sin(i * amptitude) * 200;
-    point(x, y);
+    let y = sin(i * amptitude) * 50;
+    vertex(x, y);
   }
+  endShape();
+
+  translate(0, 150);
+  beginShape();
+  for (let i = 0; i < 359; i++) {
+    let x = map(i, 0, 359, 0, width);
+    let y = sin(i) * 50;
+    vertex(x, y);
+  }
+  endShape();
+
+  translate(0, 150);
+  beginShape();
+  for (let i = 0; i < 359; i++) {
+    let x = map(i, 0, 359, 0, width);
+    let y = sin(i * amptitude) * 50 + sin(i) * 50;
+    vertex(x, y);
+  }
+  endShape();
 }
