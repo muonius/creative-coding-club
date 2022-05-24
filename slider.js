aaba763b2cdf6f5bf1e4d8461fd5b0bd840e0ac7
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-24 16:28:15 -0400

    189

--- slider.js
+++ slider.js
@@ -1,11 +1,11 @@
 // require https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js
 
-let freq_slider;
+// let freq_slider;
 let amplitude = 1;
 
 function setup() {
   createCanvas(800, 800);
-  freq_slider = createSlider(1, 200, 150, 1);
+  // freq_slider = createSlider(120, 200, 150, 1);
 
   angleMode(DEGREES);
 }
@@ -13,13 +13,14 @@ function setup() {
 function draw() {
   background(50);
   ellipseMode(CENTER);
-  let frequency = freq_slider.value();
+  // let frequency = freq_slider.value();
+  let frequency = 189;
   fill(255);
   noStroke();
 
   translate(width / 2, height / 3);
 
-  for (i = 0; i < 360 * 1; i += 0.08) {
+  for (i = 0; i < 361; i += 0.08) {
     let r1 = map(sin(i), -1, 1, 0, 100);
     let r2 = map(sin(i), -1, 1, 0, 200);
 
@@ -30,7 +31,8 @@ function draw() {
     let offsetX = cos(i * frequency + frameCount) * amplitude * 100;
     let offsetY = sin(i * frequency + frameCount) * amplitude * 100;
 
-    ellipse(x + offsetX, y + offsetY, 3, 3);
+    ellipse(x + offsetX, y + offsetY, 2, 2);
     frequency += 0.000001;
+    // console.log(frequency);
   }
 }
