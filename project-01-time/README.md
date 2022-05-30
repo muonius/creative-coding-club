bd39db5838ec6269469d21ae19c1e586227ddcc4
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-30 16:26:13 -0400

    update

---
+++ project-01-time/README.md
@@ -0,0 +1,11 @@
+# About Time
+
+### Space and Time is An Illusion
+
+In this exercise, I continue to explore the magical power of periodic functions. I want to illustrate that space and time is an illusion, and has an organic and fluid nature.
+
+My idea starts off by creating sin waves on a polar coordinate system and then adding another layer of waves. I further tweak the visual effect so the spinning sphere looks like earth, and the oscillating waves are dimentional space and time that pass through us.
+
+Finally, I add in a frequency slider to showcase the visual variations. The animation is tied to `frameCount`.
+<img src="./screenshot.png" width="600" alt="Time Screenshot">
+**[Open Processing](https://openprocessing.org/sketch/1584180)**
--- project-01-time/practice/final.js
+++ project-01-time/practice/final.js
@@ -1,14 +1,11 @@
 // require https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js
 
-let freq_slider;
+// let freq_slider;
 let amplitude = 1;
 
 function setup() {
   createCanvas(800, 800);
-  freq_slider = createSlider(186, 250, 223, 1);
-  freq_slider.position(42, 80);
-  freq_slider.addClass("mySliders");
-
+  // freq_slider = createSlider(120, 200, 150, 1);
   frameRate(60);
   angleMode(DEGREES);
 }
@@ -16,17 +13,14 @@ function setup() {
 function draw() {
   background(50);
   ellipseMode(CENTER);
-  let frequency = freq_slider.value();
-
-  fill(255);
-  text("FREQUENCY", 40, 50);
-  textSize(18);
-  textFont("Futura");
-
-  translate(width / 2, height / 3);
+  // let frequency = freq_slider.value();
+  let frequency = 189;
   fill(255);
   noStroke();
-  for (i = 0; i < 361; i += 0.07) {
+
+  translate(width / 2, height / 3);
+
+  for (i = 0; i < 361; i += 0.08) {
     let r1 = map(sin(i), -1, 1, 0, 100);
     let r2 = map(sin(i), -1, 1, 0, 200);
 
