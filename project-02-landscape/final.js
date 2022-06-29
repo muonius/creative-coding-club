936c661e515d59d101f10a494339473053b285bb
Yang Zhao <freda.y.zhao@gmail.com>
2022-06-29 19:35:05 -0400

    landscape

--- project-02-landscape/final.js
+++ project-02-landscape/final.js
@@ -1,44 +1,64 @@
 // require https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js
-
-let freq_slider;
-let amplitude = 1;
+let angle = 0;
+let offset = 0.03;
+let amptitude1 = 3;
+let amptitude2 = 6;
+let amptitude3 = 7;
 
 function setup() {
-  createCanvas(800, 800);
-  freq_slider = createSlider(186, 250, 223, 1);
-  freq_slider.position(42, 80);
-  freq_slider.addClass("mySliders");
-
-  frameRate(60);
-  angleMode(DEGREES);
+  createCanvas(600, 600, WEBGL);
+  land = createGraphics(600, 600);
+  land.fill(255);
+  // angleMode(DEGREES);
 }
 
 function draw() {
-  background(50);
-  ellipseMode(CENTER);
-  let frequency = freq_slider.value();
+  background(0);
+  land.translate(width / 2, height / 2);
+  land.noFill();
+  land.stroke(255);
+  land.strokeWeight(0.1);
+  land.beginShape();
+  for (let i = 0; i < 361; i++) {
+    let r1Min = map(sin(frameCount), -1, 1, 60, 100);
+    let r1Max = map(sin(frameCount), -1, 1, 100, 0);
 
-  fill(255);
-  text("FREQUENCY", 40, 50);
-  textSize(18);
-  textFont("Futura");
+    let r2Min = map(sin(frameCount), -1, 1, 100, 60);
+    let r2Max = map(sin(frameCount), -1, 1, 0, 100);
 
-  translate(width / 2, height / 3);
-  fill(255);
-  noStroke();
-  for (i = 0; i < 361; i += 0.07) {
-    let r1 = map(sin(i), -1, 1, 0, 100);
-    let r2 = map(sin(i), -1, 1, 0, 200);
+    let r3Min = map(sin(frameCount), -1, 1, 60, 160);
+    let r3Max = map(sin(frameCount), -1, 1, 200, 10);
 
-    let r = r1 + r2;
+    let r1 = map(sin(i * amptitude1), -1, 1, r1Min, r1Max);
+    let r2 = map(sin(i * amptitude2), -1, 1, r2Min, r2Max);
+    let r3 = map(sin(i * amptitude3 + 120), -1, 1, r3Min, r3Max);
+
+    let r = r1 + r2 + r3;
+    // let r = r1 + r2;
     let x = r * cos(i);
     let y = r * sin(i);
+    land.vertex(x, y);
+  }
+  land.endShape(CLOSE);
 
-    let offsetX = cos(i * frequency + frameCount) * amplitude * 100;
-    let offsetY = sin(i * frequency + frameCount) * amplitude * 100;
+  ambientLight(200);
+  // directionalLight(255, 255, 255, 1, 0, 1);
 
-    ellipse(x + offsetX, y + offsetY, 2, 2);
-    frequency += 0.000001;
-    // console.log(frequency);
+  rectMode(CENTER);
+
+  for (let x = -300; x < 300; x += 100) {
+    push();
+    translate(x, 0, x + 200);
+    rotateX(angle * 0.7);
+    // translate(0, 50, 0);
+    rotateY(angle * 0.3 + offset);
+    rotateZ(angle * 0.2);
+    noStroke();
+    texture(land);
+    sphere(100);
+    // box(100);
+    pop();
+    angle += 0.001;
+    offset += 0.001;
   }
 }
